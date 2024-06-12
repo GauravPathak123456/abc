865269ef61f98a105451f4668b02553c053a85d9
GauravPathak123456 <147998414+GauravPathak123456@users.noreply.github.com>
2024-06-12 15:52:47 +0530

    Add files via upload

---
+++ efg.js
@@ -0,0 +1,20 @@
+function greetUser() {
+    const now = new Date();
+    const hour = now.getHours();
+  
+    let greeting;
+  
+    if (hour < 12) {
+      greeting = "Good Morning";
+    } else if (hour < 18) {
+      greeting = "Good Afternoon";
+    } else if (hour < 21) {
+      greeting = "Good Evening";
+    } else {
+      greeting = "Good Night";
+    }
+  
+    console.log(greeting);
+  }
+  
+  greetUser();
